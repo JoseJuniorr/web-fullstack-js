cfe4191caec9e6871313c1a07f88c8c38724d6cf
JoseJuniorr <jjerrorama@gmail.com>
2020-12-04 21:18:41 -0300

    accounts tests

---
+++ backend/accounts-service/__tests__/accounts.test.ts
@@ -0,0 +1,63 @@
+import { Response } from "express";
+import supertest from "supertest";
+import app from "./../src/app";
+
+describe("testando rotas do accounts-service", () => {
+  it("GET /accounts/ - Retorna statusCode 200", async () => {
+    const resultado = await supertest(app).get("/accounts/");
+
+    expect(resultado.status).toEqual(200);
+
+    expect(Array.isArray(resultado.body)).toBeTruthy;
+  });
+
+  it("POST /accounts/ - Deve retornar status 201", async () => {
+    const payload = {
+      id: 1,
+      name: "Junior",
+      email: "jjerrorama@gmail.com",
+      password: "123654654",
+      status: 200
+      
+    };
+
+    const resultado = await supertest(app).post("/accounts/").send(payload);
+
+    expect(resultado.status).toEqual(201);
+     expect(resultado.body.id).toBe(1);
+  });
+
+  it("POST /accounts/ - Deve retornar statusCode 422", async () => {
+    const payload = {
+      sa: "1",
+      street: "Av perimetral",
+      cidade: "campo mourão",
+    };
+
+    const resultado = await supertest(app).post("/accounts/").send(payload);
+
+    expect(resultado.status).toEqual(422);
+    
+  });
+
+  it("GET /accounts/:id - Deve retornar statusCode 200", async () => {
+    const resultado = await supertest(app).get("/accounts/1");
+
+    expect(resultado.status).toEqual(200);
+    expect(resultado.body.id).toBe(1);
+  });
+
+  it("GET /accounts/:id - Deve retornar statusCode 404", async () => {
+    const resultado = await supertest(app).get("/accounts/2");
+
+    expect(resultado.status).toEqual(404);
+    
+  });
+
+  it("GET /accounts/:id - Deve retornar statusCode 400", async () => {
+    const resultado = await supertest(app).get("/accounts/abc");
+
+    expect(resultado.status).toEqual(400);
+    
+  });
+});
