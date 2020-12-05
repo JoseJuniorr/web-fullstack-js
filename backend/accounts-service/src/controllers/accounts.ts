01d1bbe14368b8cb8edb851017e79c908d7688ab
JoseJuniorr <jjerrorama@gmail.com>
2020-12-04 21:17:18 -0300

    accounts functions, getAccounts, getAccountsById, addAccount, routes and middleware validation schema model

--- backend/accounts-service/src/controllers/accounts.ts
+++ backend/accounts-service/src/controllers/accounts.ts
@@ -7,6 +7,24 @@ function getAccounts(req: Request, res: Response, next: any) {
   res.json(accounts);
 }
 
+function getAccountById(req: Request, res: Response, next: any) {
+  try {
+    const id = parseInt(req.params.id);
+    if (!id) throw new Error("ID invalid format");
+
+    const index = accounts.findIndex((item) => item.id === id);
+
+    if (index === -1) {
+      return res.status(404).end();
+    } else {
+      res.json(accounts[index]);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(400).end();
+  }
+}
+
 function addAccount(req: Request, res: Response, next: any) {
   try {
     const newAccount = req.body as IAccount;
@@ -18,4 +36,4 @@ function addAccount(req: Request, res: Response, next: any) {
   }
 }
 
-export default { getAccounts, addAccount };
+export default { getAccounts, addAccount, getAccountById };
--- backend/accounts-service/src/models/account.ts
+++ backend/accounts-service/src/models/account.ts
@@ -1,7 +1,20 @@
+import { AccountStatus } from "./accountsStatus";
+import Joi from "joi";
+
 export interface IAccount {
   id: number;
   name: string;
   email: string;
   password: string;
-  status: number;
+  status: AccountStatus;
 }
+
+const accountSchema = Joi.object({
+  id: Joi.number().integer().min(1),
+  name: Joi.string().alphanum().min(3).max(150).required(),
+  email: Joi.string().email().min(6).max(150).required(),
+  password: Joi.string().min(6).max(50),
+  status: Joi.number().integer().min(100).max(400),
+});
+
+export { accountSchema };
