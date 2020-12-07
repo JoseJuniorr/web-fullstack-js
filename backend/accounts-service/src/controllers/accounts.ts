a495a1035e39d8d1afba8625ab86f4ddbb2c9975
José Junior <jjerrorama@gmail.com>
2020-12-07 11:05:10 -0300

    added and updated functions getAccountById, setAccount, loginAccount, logoutAccount

--- backend/accounts-service/src/controllers/accounts.ts
+++ backend/accounts-service/src/controllers/accounts.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+
 import { IAccount } from "./../models/account";
 
 const accounts: IAccount[] = [];
@@ -36,4 +37,51 @@ function addAccount(req: Request, res: Response, next: any) {
   }
 }
 
-export default { getAccounts, addAccount, getAccountById };
+function setAccount(req: Request, res: Response, next: any) {
+  try {
+    const id = parseInt(req.params.id);
+    if (!id) throw new Error("ID is invalid format.");
+    const accountParams = req.body as IAccount;
+    const index = accounts.findIndex((item) => item.id === id);
+    const originalAccount = accounts[index];
+
+    if (index === -1) return res.status(404).end();
+
+    if (accountParams.name) originalAccount.name = accountParams.name;
+    if (accountParams.password)
+      originalAccount.password = accountParams.password;
+
+    accounts[index] = originalAccount;
+    res.status(200).end();
+  } catch (error) {
+    console.log(error);
+    res.status(400).end();
+  }
+}
+
+function loginAccount(req: Request, res: Response, next: any) {
+  try {
+    const loginParams = req.body as IAccount;
+    const index = accounts.findIndex(
+      (item) =>
+        item.email === loginParams.email &&
+        item.password === loginParams.password
+    );
+    if (index === -1) res.status(401).end();
+
+    res.json({ auth: true, token: {} });
+  } catch (error) {}
+}
+
+function logoutAccount(req: Request, res: Response, next: any) {
+  res.json({ auth: false, token: null });
+}
+
+export default {
+  getAccounts,
+  addAccount,
+  getAccountById,
+  setAccount,
+  loginAccount,
+  logoutAccount,
+};
