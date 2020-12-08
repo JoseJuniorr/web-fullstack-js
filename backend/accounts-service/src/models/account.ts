ab38269df2ad0659c2f7d6b6e40f0c863ab04b6d
JoseJuniorr <jjerrorama@gmail.com>
2020-12-08 16:26:35 -0300

    extracted schema validation for account schema

--- backend/accounts-service/src/models/account.ts
+++ backend/accounts-service/src/models/account.ts
@@ -1,26 +1,10 @@
 import { AccountStatus } from "./accountsStatus";
-import Joi from "joi";
 
 export interface IAccount {
-  id: number;
+  id?: number;
   name: string;
   email: string;
   password: string;
   status: AccountStatus;
-  domain: string
+  domain: string;
 }
-
-const accountSchema = Joi.object({
-  id: Joi.number().integer().min(1),
-  name: Joi.string().min(3).max(150).required(),
-  email: Joi.string().email().min(6).max(150).required(),
-  password: Joi.string().min(6).max(50),
-  status: Joi.number().integer().min(100).max(400),
-});
-
-const loginSchema = Joi.object({
-  email: Joi.string().email().min(6).max(150).required(),
-  password: Joi.string().min(6).max(50),
-});
-
-export { accountSchema, loginSchema };
