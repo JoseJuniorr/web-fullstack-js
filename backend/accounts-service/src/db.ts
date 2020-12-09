4d6d8ecf2c0ba27dcd8c9c9c13dc832f21336999
JoseJuniorr <jjerrorama@gmail.com>
2020-12-09 19:32:17 -0300

    enable or disable sql log

--- backend/accounts-service/src/db.ts
+++ backend/accounts-service/src/db.ts
@@ -5,9 +5,12 @@ const dbUser = process.env.DB_USER!;
 const dbPassword = process.env.DB_PASSWORD;
 const dbHost = process.env.DB_HOST;
 
+const logging = process.env.SQL_LOG ? true : false;
+
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   dialect: "mysql",
   host: dbHost,
+  logging: logging,
 });
 
 export default sequelize;
