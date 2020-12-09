f0dc370a564f2ad83d46fe071463f259c1e1b30d
JoseJuniorr <jjerrorama@gmail.com>
2020-12-09 19:26:43 -0300

    extracted functions from repository to repositoryModel

--- backend/accounts-service/src/models/accountModel.ts
+++ backend/accounts-service/src/models/accountModel.ts
@@ -5,11 +5,11 @@ import { IAccount } from "./account";
 
 interface AccountCreationAttributes extends Optional<IAccount, "id"> {}
 
-export interface AccountModel
+export interface IAccountModel
   extends Model<IAccount, AccountCreationAttributes>,
     IAccount {}
 
-const accountModel = database.define<AccountModel>("account", {
+export default database.define<IAccountModel>("account", {
   id: {
     type: Sequelize.INTEGER.UNSIGNED,
     primaryKey: true,
@@ -39,36 +39,3 @@ const accountModel = database.define<AccountModel>("account", {
     allowNull: true,
   },
 });
-
-function findAll() {
-  return accountModel.findAll<AccountModel>();
-}
-
-function findByEmail(emailFilter: string) {
-  return accountModel.findOne<AccountModel>({ where: { email: emailFilter } });
-}
-
-function findById(id: number) {
-  return accountModel.findByPk<AccountModel>(id);
-}
-
-function addAccount(account: IAccount) {
-  return accountModel.create(account);
-}
-
-async function set(id: number, account: IAccount) {
-  const originalAccount = await accountModel.findByPk<AccountModel>(id);
-
-  if (originalAccount !== null) {
-    originalAccount.name = account.name;
-    originalAccount.domain = account.domain;
-    originalAccount.status = account.status;
-    if (!account.password) originalAccount.password = account.password;
-
-    return await originalAccount.save();
-    return originalAccount;
-  }
-  throw new Error(`Account not found.`);
-}
-
-export default { findAll, findById, addAccount, set, findByEmail };
