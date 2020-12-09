57a294b616d4c3989d02db1096d959c4a7485af4
JoseJuniorr <jjerrorama@gmail.com>
2020-12-09 19:28:57 -0300

    refactored tests after implementation of methods with connection to the database

--- backend/accounts-service/__tests__/accounts.auth.ts
+++ backend/accounts-service/__tests__/accounts.auth.ts
@@ -1,23 +1,35 @@
 import supertest from "supertest";
 import app from ".././src/app";
+import { IAccount } from "../src/models/account";
+
+import repository from "../src/models/accountRepository";
+
+const testEmail = "jest@gmail.com";
+const hashPassword =
+  "$2a$10$xPMrrzxd7w4doT5bQM7cMOVpZ3vTpo8Pag2qF8xO5m17tPVk1GQR6"; //123654
+const testPassword = "123654";
+
+beforeAll(async () => {
+  const testAccount: IAccount = {
+    name: "jest",
+    email: testEmail,
+    password: hashPassword,
+    domain: "jest.com",
+  };
+
+  await repository.addAccount(testAccount);
+});
+
+afterAll(async () => {
+  await repository.removeByEmail(testEmail);
+});
 
 describe("Testando rotas de autenticação", () => {
   it("POST /accounts/login - 200 OK", async () => {
-    //mocking
-    const newAccount = {
-      id: 1,
-      name: "Jose Junior",
-      email: "jjerrorama@gmail.com",
-      password: "123654",
-      status: 100,
-    };
-
-    await supertest(app).post("/accounts/").send(newAccount);
-
     //testing
     const payload = {
-      email: "jjerrorama@gmail.com",
-      password: "123654",
+      email: testEmail,
+      password: testPassword,
     };
 
     const resultado = await supertest(app)
@@ -31,8 +43,8 @@ describe("Testando rotas de autenticação", () => {
 
   it("POST /accounts/login - 401 Unauthorized", async () => {
     const payload = {
-      email: "jjerrorama@gmail.com",
-      password: "abcabcas",
+      email: testEmail,
+      password: testPassword + "1",
     };
 
     const resultado = await supertest(app)
@@ -44,8 +56,9 @@ describe("Testando rotas de autenticação", () => {
 
   it("POST /accounts/login - 422 Unprocessable Entity", async () => {
     const payload = {
-      email: "jjerrorama",
-      password: "abcabcas",
+      email: testEmail,
+      password: ''
+      
     };
 
     const resultado = await supertest(app)
--- backend/accounts-service/__tests__/accounts.test.ts
+++ backend/accounts-service/__tests__/accounts.test.ts
@@ -1,9 +1,43 @@
 import supertest from "supertest";
+import { IAccount } from "../src/models/account";
 import app from "./../src/app";
+
+import repository from "../src/models/accountRepository";
+
+import auth from "../src/auth";
+
+const testEmail = "jest@gmail.com";
+const testEmail2 = "jest2@gmail.com";
+const hashPassword =
+  "$2a$10$xPMrrzxd7w4doT5bQM7cMOVpZ3vTpo8Pag2qF8xO5m17tPVk1GQR6"; //123654
+const testPassword = "123654";
+
+let jwt: string = "";
+let testId: number = 0;
+
+beforeAll(async () => {
+  const testAccount: IAccount = {
+    name: "jest",
+    email: testEmail,
+    password: hashPassword,
+    domain: "jest.com",
+  };
+
+  const result = await repository.addAccount(testAccount);
+  testId = result.id!;
+  jwt = auth.sign(result.id!);
+});
+
+afterAll(async () => {
+  await repository.removeByEmail(testEmail);
+  await repository.removeByEmail(testEmail2);
+});
 
 describe("testando rotas do accounts-service", () => {
   it("GET /accounts/ - Retorna statusCode 200", async () => {
-    const resultado = await supertest(app).get("/accounts/");
+    const resultado = await supertest(app)
+      .get("/accounts/")
+      .set("x-access-token", jwt);
 
     expect(resultado.status).toEqual(200);
 
@@ -11,23 +45,22 @@ describe("testando rotas do accounts-service", () => {
   });
 
   it("POST /accounts/ - Deve retornar status 201", async () => {
-    const payload = {
-      id: 1,
-      name: "Junior",
-      email: "jjerrorama@gmail.com",
-      password: "123654654",
-      status: 200,
+    const payload: IAccount = {
+      name: "jest2",
+      email: testEmail2,
+      password: "123654",
+
+      domain: "jest.dev.br",
     };
 
     const resultado = await supertest(app).post("/accounts/").send(payload);
 
     expect(resultado.status).toEqual(201);
-    expect(resultado.body.id).toBe(1);
+    expect(resultado.body.id).toBeTruthy();
   });
 
   it("POST /accounts/ - Deve retornar statusCode 422", async () => {
     const payload = {
-      sa: 1,
       street: "Av perimetral",
       cidade: "campo mourão",
     };
@@ -40,24 +73,27 @@ describe("testando rotas do accounts-service", () => {
   it("PATCH /accounts/:id - Deve retornar statusCode 200", async () => {
     const payload = {
       name: "Jose Junior",
-      email: "jjerrorama2@gmail.com",
-      password: "123654",
     };
 
-    const resultado = await supertest(app).patch("/accounts/1").send(payload);
+    const resultado = await supertest(app)
+      .patch("/accounts/" + testId)
+      .send(payload)
+      .set("x-access-token", jwt);
 
     expect(resultado.status).toEqual(200);
-    // expect(resultado.body.id).toEqual(1);
+    // expect(resultado.body.id).toEqual(testId);
+    // expect(resultado.body.name).toEqual(payload.name);
   });
 
   it("PATCH /accounts:id/ - Deve retornar statusCode 400", async () => {
     const payload = {
       name: "Jose Junior",
-      email: "jjerrorama@gmail.com",
-      password: "123654654@",
     };
 
-    const resultado = await supertest(app).patch("/accounts/abc").send(payload);
+    const resultado = await supertest(app)
+      .patch("/accounts/abc")
+      .send(payload)
+      .set("x-access-token", jwt);
 
     expect(resultado.status).toEqual(400);
   });
@@ -65,31 +101,34 @@ describe("testando rotas do accounts-service", () => {
   it("PATCH /accounts:id/ - Deve retornar statusCode 404", async () => {
     const payload = {
       name: "Jose Junior",
-      email: "jjerrorama@gmail.com",
-      password: "123654654@",
     };
 
-    const resultado = await supertest(app).patch("/accounts/-1").send(payload);
+    const resultado = await supertest(app)
+      .patch("/accounts/-1")
+      .send(payload)
+      .set("x-access-token", jwt);
 
     expect(resultado.status).toEqual(404);
   });
 
   it("GET /accounts/:id - Deve retornar statusCode 200", async () => {
-    const resultado = await supertest(app).get("/accounts/1");
+    const resultado = await supertest(app)
+      .get("/accounts/" + testId)
+      .set("x-access-token", jwt);
 
     expect(resultado.status).toEqual(200);
-    expect(resultado.body.id).toBe(1);
+    expect(resultado.body.id).toBe(testId);
   });
 
-  it("GET /accounts/:id - Deve retornar statusCode 404", async () => {
-    const resultado = await supertest(app).get("/accounts/2");
+    it("GET /accounts/:id - Deve retornar statusCode 404", async () => {
+      const resultado = await supertest(app).get("/accounts/-1").set("x-access-token", jwt);
 
-    expect(resultado.status).toEqual(404);
-  });
+      expect(resultado.status).toEqual(404);
+    });
 
-  it("GET /accounts/:id - Deve retornar statusCode 400", async () => {
-    const resultado = await supertest(app).get("/accounts/abc");
+    it("GET /accounts/:id - Deve retornar statusCode 400", async () => {
+      const resultado = await supertest(app).get("/accounts/abc").set("x-access-token", jwt);
 
-    expect(resultado.status).toEqual(400);
-  });
+      expect(resultado.status).toEqual(400);
+    });
 });
