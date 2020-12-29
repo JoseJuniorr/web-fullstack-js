fa83d594d9790a223625b011f18c76a2e3c17feb
José Junior <jjerrorama@gmail.com>
2020-12-28 22:21:06 -0300

    cors added

--- backend/__commons__/src/api/app.ts
+++ backend/__commons__/src/api/app.ts
@@ -1,11 +1,15 @@
 import express, { Router } from "express";
 import bodyParser from "body-parser";
 import helmet from "helmet";
+import cors from "cors";
 
 export default (router: Router) => {
   const app = express();
 
   app.use(helmet());
+  const corstOpts = cors({ origin: true });
+  app.use(corstOpts);
+
   app.use(bodyParser.json());
 
   app.use(router);
